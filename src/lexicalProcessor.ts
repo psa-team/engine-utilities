328a321aed130a7675c9f2e1134353ecdb49e574
Sergey Minyurov <minyurov@gmail.com>
2022-07-18 21:52:42 +0300

    add methods to lexicalProcessor

--- src/lexicalProcessor.ts
+++ src/lexicalProcessor.ts
@@ -1,19 +1,28 @@
-export interface LexicalFunction
+export interface ILexicalAction
 {
-    (prop: string, value: any, props: string[], values: any[]) : boolean;
+    (prop: string, value: any, props: string[], values: any[]) : void;
+}
+export interface ILexicalFilter
+{
+    (prop: string, value: any, props?: string[], values?: any[]) : boolean;
+}
+export interface IKeyValuePair
+{
+    key: string;
+    value: string
 }
 
 export class LexicalProcessor
 {
     constructor(public readonly json: string) { };
 
-    forEach(action: LexicalFunction, filter?: LexicalFunction): void
+    forEach(action: ILexicalAction, filter?: ILexicalFilter): void
     {
         let props: string[] = [];
         let values: string[] = [];
         this.forEachImpl(this.json, props, values, action, filter);
     }
-    private forEachImpl(data: string, props: string[], values: any[], action: LexicalFunction, filter?: LexicalFunction): void
+    private forEachImpl(data: string, props: string[], values: any[], action: ILexicalAction, filter?: ILexicalFilter): void
     {
         for (const[prop, value] of Object.entries(data))
         {
@@ -31,5 +40,84 @@ export class LexicalProcessor
             props.pop();
             values.pop();
         }
+    }
+
+    select(filter: ILexicalFilter, data?: string) : any[]
+    {
+        let props: string[] = [];
+        let values: string[] = [];
+        let result: any[] = [];
+        this.selectImpl(data ?? this.json, props, values, result, filter);
+        return result;
+    }
+    private selectImpl(data: string, props: string[], values: any[], result: any[], filter: ILexicalFilter) : any[]
+    {
+        for (const[prop, value] of Object.entries(data))
+        {
+            if (prop === '0')
+            {
+                break;
+            }
+            if (filter(prop, value, props, values))
+            {
+                result.push(value);
+            }
+            props.push(prop);
+            values.push(value);
+            this.selectImpl(value, props, values, result, filter);
+            props.pop();
+            values.pop();
+        }
+        return result;
+    }
+
+    first(filter: ILexicalFilter, data?: string) : any
+    {
+        let props: string[] = [];
+        let values: string[] = [];
+        return this.firstImpl(data ?? this.json, props, values, filter);
+    }
+    private firstImpl(data: string, props: string[], values: string[], filter: ILexicalFilter) : any
+    {
+        for (const[prop, value] of Object.entries(data))
+        {
+            if (prop === '0')
+            {
+                break;
+            }
+            if (filter(prop, value, props, values))
+            {
+                return value;
+            }
+            props.push(prop);
+            values.push(value);
+            var result = this.firstImpl(value, props, values, filter);
+            if (result)
+            {
+                return result;
+            }
+            props.pop();
+            values.pop();
+        }
+        return null;
+    }
+
+    layer(data: string, filter?: ILexicalFilter) : IKeyValuePair[]
+    {
+        let result: IKeyValuePair[] = [];
+        for (const[prop, value] of Object.entries(data))
+        {
+            if (prop === '0') {
+                break;
+            }
+            if (!filter || filter(prop, value)) {
+                let pair: IKeyValuePair = {
+                    key: prop,
+                    value: value
+                }
+                result.push(pair);
+            }
+        }
+        return result;
     }
 }
--- test/lexicalProcessor.test.ts
+++ test/lexicalProcessor.test.ts
@@ -1,9 +1,16 @@
 import { LexicalProcessor } from "../src/lexicalProcessor";
 
-const json = "";
+const json: string = require('./generalMedicalPerspective');
 
 describe("test add function", () => {
     it("should return 15 for add(10,5)", () => {
         expect(1).toBe(1);
     })
+});
+
+test('should find startView', () => {
+    const proc = new LexicalProcessor(json);
+    var counter: number = 0;
+    proc.forEach(() => { counter++; }, (prop) => prop === 'startView');
+    expect(counter).toEqual(1);
 });
